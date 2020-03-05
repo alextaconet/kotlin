b45a36abfce6ab5aa62f16023bfe303db4c791e4
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-03-05 15:28:14 +0300

    FIR: add & use argument mapping in function calls #KT-36345 Fixed

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/FirCallResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/FirCallResolver.kt
@@ -84,6 +84,7 @@ class FirCallResolver(
 
         val resultExpression = functionCall.transformCalleeReference(StoreNameReference, nameReference)
         val candidate = resultExpression.candidate()
+        resultExpression.replaceArgumentMap(candidate?.argumentMapping)
 
         // We need desugaring
         val resultFunctionCall = if (candidate != null && candidate.callInfo != result.info) {
@@ -93,6 +94,7 @@ class FirCallResolver(
                 extensionReceiver = candidate.extensionReceiverExpression(),
                 arguments = candidate.callInfo.arguments,
                 safe = candidate.callInfo.isSafeCall,
+                argumentMap = candidate.argumentMapping,
             )
         } else {
             resultExpression
